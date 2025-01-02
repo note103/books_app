d5f7d87c1fea0dd513bdd63a4bdf45d078604945
Hiroaki Kadomatsu <note103@gmail.com>
2025-01-03 01:05:08 +0900

    authorカラムの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.expect(book: [ :title, :memo ])
+      params.require(:book).permit(:title, :memo, :author)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_30_151106) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_02_150944) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 end
