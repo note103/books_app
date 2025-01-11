807eb4c1928555ed84c28c67c2ad8c269a1e9348
Hiroaki Kadomatsu <note103@gmail.com>
2025-01-12 02:57:25 +0900

    Add author column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_07_161607) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_121715) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 end
