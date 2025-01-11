03e7d7fed2731b833ef7275a96d3cf9f8fd766b5
Hiroaki Kadomatsu <note103@gmail.com>
2025-01-12 02:59:13 +0900

    Add image column to books table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.expect(book: [ :title, :memo ])
+      params.expect(book: [ :title, :memo, :author, :picture ])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_121715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 end
